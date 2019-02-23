8fba1d90a0626940827b9cb5813e37f13a90b7cd
jinhesui <jinhesui@163.com>
2019-02-23 10:50:09 +0000

    基础页面结构

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="container">
+    <p class="text-center">
+      由 <a href="https://weibo.com/u/1648283277?is_hot=1" target="_blank">jinhesui</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+      © 2019 果壳机动 <a href="http://www.miitbeian.gov.cn/" target="_blank">京ICP备18058797号</a>
+      <a href="mailto:jinhesui@163.com">联系我</a>
+    </p>
+  </div>
+</footer>
