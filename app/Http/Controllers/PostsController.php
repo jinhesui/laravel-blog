5bad6a9e6bcd5133c225542c8b48cdf9daf49cbc
jinhesui <jinhesui@163.com>
2019-02-24 04:35:06 +0000

    修复 N+1 问题

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -16,7 +16,7 @@ class PostsController extends Controller
 
 	public function index()
 	{
-		$posts = Post::paginate();
+		$posts = Post::with('user', 'category')->paginate(10);
 		return view('posts.index', compact('posts'));
 	}
 
